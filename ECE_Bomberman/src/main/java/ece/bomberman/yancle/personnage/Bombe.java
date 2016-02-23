e272010ee72f61f124aa00dd64ba2f34972be360
ClMatthey <matthey.clement@hotmail.fr>
2016-02-23 11:08:37 +0100

    Merge

---
+++ ECE_Bomberman/src/main/java/ece/bomberman/yancle/personnage/Bombe.java
@@ -0,0 +1,72 @@
+package ece.bomberman.yancle.personnage;
+
+public class Bombe {
+	private int timer;
+	private int power;
+	private int x;
+	private int y;
+	
+	public Bombe(int tim, int pow, int x, int y){
+		setTimer(tim);
+		setPower(pow);
+		setX(x);
+		setY(y);
+	}
+
+	/**
+	 * @return the timer
+	 */
+	public int getTimer() {
+		return timer;
+	}
+
+	/**
+	 * @param timer the timer to set
+	 */
+	public void setTimer(int timer) {
+		this.timer = timer;
+	}
+
+	/**
+	 * @return the power
+	 */
+	public int getPower() {
+		return power;
+	}
+
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
---
+++ ECE_Bomberman/src/main/java/ece/bomberman/yancle/personnage/Personnage.java
@@ -0,0 +1,120 @@
+package ece.bomberman.yancle.personnage;
+
+import java.util.HashSet;
+
+public class Personnage {
+	private int life;
+	private int timer;
+	private int power;
+	private int numberMaxOfBombe;
+	private HashSet<Bombe> bombSet;
+	private int x;
+	private int y;
+	
+	public Personnage(){
+		life = 5;
+		numberMaxOfBombe = 3;
+		bombSet = new HashSet<Bombe>();
+		timer = 4;
+		power = 2;
+	}
+
+	/**
+	 * @return the life
+	 */
+	public int getLife() {
+		return life;
+	}
+
+	/**
+	 * @param life the life to set
+	 */
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	/**
+	 * @return the timer
+	 */
+	public int getTimer() {
+		return timer;
+	}
+
+	/**
+	 * @param timer the timer to set
+	 */
+	public void setTimer(int timer) {
+		this.timer = timer;
+	}
+
+	/**
+	 * @return the power
+	 */
+	public int getPower() {
+		return power;
+	}
+
+	/**
+	 * @param power the power to set
+	 */
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	/**
+	 * @return the numberMaxOfBombe
+	 */
+	public int getNumberMaxOfBombe() {
+		return numberMaxOfBombe;
+	}
+
+	/**
+	 * @param numberMaxOfBombe the numberMaxOfBombe to set
+	 */
+	public void setNumberMaxOfBombe(int numberMaxOfBombe) {
+		this.numberMaxOfBombe = numberMaxOfBombe;
+	}
+
+	/**
+	 * @return the bombSet
+	 */
+	public HashSet<Bombe> getBombSet() {
+		return bombSet;
+	}
+
+	/**
+	 * @param bombSet the bombSet to set
+	 */
+	public void setBombSet(HashSet<Bombe> bombSet) {
+		this.bombSet = bombSet;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
