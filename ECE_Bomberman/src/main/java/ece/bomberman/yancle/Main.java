2d245421feea446071cea0cc04eed78f2c02ddd5
Yan PIERRU <yan.pierru@laposte.net>
2016-02-23 10:48:36 +0100

    First frame (without controls)

---
+++ ECE_Bomberman/src/main/java/ece/bomberman/yancle/Main.java
@@ -0,0 +1,42 @@
+package ece.bomberman.yancle;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private Stage stage;
+    private AnchorPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		stage = primaryStage;
+		stage.setResizable(false);
+		stage.setTitle("Bomberman");
+		
+
+        try {
+        // Load root layout from fxml file.
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/StartFrame.fxml"));
+        rootLayout = (AnchorPane) loader.load();
+
+        // Show the scene containing the root layout.
+        Scene scene = new Scene(rootLayout);
+        stage.setScene(scene);
+        stage.show();
+        
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ ECE_Bomberman/src/main/java/ece/bomberman/yancle/view/StartFrameController.java
@@ -0,0 +1,27 @@
+package ece.bomberman.yancle.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class StartFrameController implements Initializable {
+
+	@FXML
+	private Button joinServerButton;
+	
+	@FXML
+	private Button startServerButton;
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+	
+	
+	
+
+}
