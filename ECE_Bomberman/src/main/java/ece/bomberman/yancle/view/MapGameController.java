60d52e56797624343f6602667976638fe0ccc293
Yan PIERRU <yan.pierru@laposte.net>
2016-02-23 12:19:31 +0100

    adding empty map

---
+++ ECE_Bomberman/src/main/java/ece/bomberman/yancle/view/MapGameController.java
@@ -0,0 +1,23 @@
+package ece.bomberman.yancle.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+public class MapGameController implements Initializable{
+
+	@FXML
+	private GridPane map;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
